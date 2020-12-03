8318ca1fde9475e569242eb699aeb7bd84fee5ac
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2020-12-03 12:05:22 +0200

    Refactored code

--- hist_equ/hist_equ/hist_equ.cpp
+++ hist_equ/hist_equ/hist_equ.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <chrono> 
 #include <math.h>
+#include <string.h>
 
 using namespace std::chrono;
 using namespace std;
@@ -75,53 +76,54 @@ void display_histogram(int histogram[], const char* name) {
 
 int main()
 {
-    //Reading input image, PUT YOUR OWN IMAGE HERE
-    Mat image = imread("../images/img.jpg", IMREAD_GRAYSCALE);
+    string image_str = "../images/img0";
+    string extension = ".jpg";
+    string img_name = image_str + extension;
+    Mat image = imread(img_name, IMREAD_GRAYSCALE);
     Mat dst;
+    int histogram[256];
+    int cumulativeHistogram[256];
 
     auto start = high_resolution_clock::now();
 
-    //Call function to create histogram
-    int histogram[256];
     compute_histogram(image, histogram);
 
-    //Get the image size
     int size = image.rows * image.cols;
     float alpha = 255.0 / size;
-    //Probability distribution for intensity levels
+
+    // Probability distribution for intensity levels
     float PRk[256];
     for (int i = 0; i < 256; i++) {
         PRk[i] = (double)histogram[i] / size;
     }
 
-    //Call function to create cumulative histogram
-    int cumulativeHistogram[256];
     compute_cumulative_histogram(histogram, cumulativeHistogram);
 
-    //Scaling operation
+    // Scaling operation
     int Sk[256];
     for (int i = 0; i < 256; i++)
     {
         Sk[i] = cvRound((double)cumulativeHistogram[i] * alpha);
     }
 
-    //Initializing equalized histogram
+    // Initializing equalized histogram
     float PSk[256];
     for (int i = 0; i < 256; i++) {
         PSk[i] = 0;
     }
-    //Mapping operation
+
+    // Mapping operation
     for (int i = 0; i < 256; i++) {
         PSk[Sk[i]] += PRk[i];
     }
 
-    //Rounding to get final values
+    // Rounding to get final values
     int finalValues[256];
     for (int i = 0; i < 256; i++) {
         finalValues[i] = cvRound(PSk[i] * 255);
     }
 
-    //Creating equalized image
+    // Creating equalized image
     Mat finalImage = image.clone();
 
     for (int y = 0; y < image.rows; y++) {
@@ -131,32 +133,23 @@ int main()
     }
 
     auto stop = high_resolution_clock::now();
-
     auto duration = duration_cast<nanoseconds>(stop - start);
 
-    cout << "Execution time CPU: " << duration.count() << endl;
-
+    cout << "Execution time CPU: " << (float)duration.count() / 1000000 << endl;
 
     start = high_resolution_clock::now();
 
     equalizeHist(image, dst);
 
     stop = high_resolution_clock::now();
-
     duration = duration_cast<nanoseconds>(stop - start);
 
-    cout << "Execution time CPU opencv: " << duration.count() << endl;
-
-    //Displaying source image
-    namedWindow("Original Image");
-    imshow("Original Image", image);
-    //Displaying source image histogram
+    cout << "Execution time CPU opencv: " << (float)duration.count() / 1000000 << endl;
+    
     display_histogram(histogram, "Original Histogram");
-    //Displaying histogram equalized image
     namedWindow("Equilized Image");
     imshow("Equilized Image", finalImage);
-    //Displaying equalized image histogram
-    display_histogram(finalValues, "Equilized Histogram");
+    display_histogram(finalValues, "Equalized Histogram");
 
     waitKey();
     return 0;
